a685f0569cf5e968726b2bf850c8c08fd150c9f8
Iacob Sergiu <iacob@a11p11.fil.univ-lille1.fr>
2020-01-24 16:38:20 +0100

    pdm work

--- pdm/tp02/sum.s
+++ pdm/tp02/sum.s
@@ -1,45 +1,39 @@
 .data
-    N:
-        .byte 4
-
-.bss
-    BUFF:
-        .space 4
+    N: .byte 4
+    BUFFER: .long 4
 
 .text
 
 .global _start
 
+
 _start:
-    call _read_int
-    xor %ecx, %ecx
-    mov $BUFF, %ecx
-    push %rcx
-    call _read_int
-    pop %rcx
-    jmp _done
-    add $BUFF, %rcx
+    call _read_in_buffer
+    mov $BUFFER, %rax
+    push (%rax)
+    call _read_in_buffer
+    pop %rax
+    addl %eax, BUFFER
+    call _write_buffer
     jmp _done
 
-_print_res:
-    mov %eax, %ecx
-    mov $4, %edx
-    mov $4, %eax
-    mov $1, %ebx
-    int $0x80
-    ret
-
-_read_int:
+_read_in_buffer:
     mov $3, %eax
     mov $0, %ebx
-    mov $BUFF, %ecx
+    mov $BUFFER, %ecx
     mov $N, %edx
     int $0x80
     ret
-    
+
+_write_buffer:
+    mov $4, %eax
+    mov $1, %ebx
+    mov $BUFFER, %ecx
+    mov $4, %edx
+    int $0x80
+    ret
+
 _done:
-    mov %ecx, %eax
-    call _print_res
-    mov $1, %eax
     mov $0, %ebx
+    mov $1, %eax
     int $0x80
