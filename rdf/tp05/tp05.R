ee95e8c4c2289fc1a049e4a1b4f8b81c160d2ba5
sergiuiacob1 <sergiu.iacob1@gmail.com>
2020-02-27 23:36:14 +0100

    RdF: work on report TP05

--- rdf/tp05/tp05.R
+++ rdf/tp05/tp05.R
@@ -45,10 +45,7 @@ calculateClassAPrioriProbabilities <- function(){
 probs <- calculateClassAPrioriProbabilities()
 print (probs)
 
-calculateConditionalProbability <- function (X){
-  # get P(w1) and P(w2)
-  probs <- calculateClassAPrioriProbabilities()
-  
+calculateConditionalProbability <- function (X, probs){
   # let's take a look at the histogram again
   # we can notice that h$counts[X + 1] gives us the number of pixels that have the gray value of X
   # the "+ 1" above is because in R arrays start from 1
@@ -72,7 +69,7 @@ calculateConditionalProbability <- function (X){
   c(px, p_x_if_w1, p_x_if_w2)
 }
 
-print (calculateConditionalProbability(141))
+print (calculateConditionalProbability(141, probs))
 
 
 
@@ -81,48 +78,55 @@ print (calculateConditionalProbability(141))
 automaticSegmentationUsingBayes <- function(){
   # calculate these probabilities only once
   probs <- calculateClassAPrioriProbabilities()
+  # only get the imageData, it will be faster for comparisons
+  image <- imageData(image)
+  # calculate these dimensions only once
+  dimImage <- dim(image)
+  dimAbove <- dim(above)
+  dimBelow <- dim(below)
   
   # build our reference segmentation
   perfect <- matrix (ncol=dim(image)[1], nrow=dim(image)[2])
   # dim(image)[1] = number of columns
-  for (i in 1:dim(above)[2])
-    for (j in 1:dim(image)[1]){
-      perfect[j, i] <- 0
+  for (i in 1:dim(image)[1])
+    for (j in 1:dim(above)[2]){
+      perfect[i, j] <- FALSE
     }
-  for (i in dim(below)[2]:dim(image)[2])
-    for (j in 1:dim(image)[1]){
-      perfect[j, i] <- 1
+  for (i in 1:dim(image)[1])
+    for (j in dim(above)[2]:dim(image)[2]){
+      perfect[i, j] <- TRUE
     }
-  
-  display(perfect)
+  display(perfect, method="raster", all=TRUE)
   
   # search for the best threshold
-  # max error is 1
-  min_error <- 1
+  # max error is (hypotethically) when all probabilities are 1 and all pixels are missclassified
+  min_error <- 2 * dimImage[1] * dimImage[2]
   best_threshold <- 0
   
-  for (X in 1:255){
-    print (X)
-    binary <- (image - X/255) > 0
+  # precalculate all probabilities
+  condProbs <- array(dim=c(dimImage[1], dimImage[2], 3))
+  for (i in 1:dimImage[1])
+    for (j in 1:dimImage[2]){
+      res <- calculateConditionalProbability(image[i, j] * 255, probs)
+      condProbs[i, j, 1] <- res[1]
+      condProbs[i, j, 2] <- res[2]
+      condProbs[i, j, 3] <- res[3]
+    }
+      
+  for (X in 0:255){
+    binary <- (image - X/255) >= 0
+    # only get the data, otherwise the comparisons will be really slow
+    binary <- imageData(binary)
     
-    print ('sum1')  
-    sum1 <- 0
-    for (i in 1:dim(above)[1])
-      for (j in 1:dim(above)[2]){
-        if (binary [i, j] != perfect [i, j])
-          sum1 <- sum1 + 0.1
-          # sum1 <- sum1 + calculateConditionalProbability(image[i, j] * 255) * probs[1]
-      }
-    print ('sum1 done')
-    
-    sum2 <- 0
-    for (i in dim(below)[1]:dim(image)[1])
-      for (j in 1:dim(below)[2]){
-        if (binary [i, j] != perfect [i, j])
-          sum1 <- sum1 + calculateConditionalProbability(image[i, j] * 255) * probs[2]
-      }
-    
-    error <- sum1 + sum2
+    error <- 0
+    for (i in 1:dimImage[1])
+      for (j in 1:dimAbove[2])
+        if (perfect [i, j] != binary [i, j])
+          error <- error + condProbs[2] * probs[1]
+    for (i in 1:dimImage[1])
+      for (j in dimAbove[2]:dimImage[2])
+        if (perfect [i, j] != binary [i, j])
+          error <- error + condProbs[3] * probs[2]
       
     if (error < min_error){
       min_error <- error
@@ -131,8 +135,9 @@ automaticSegmentationUsingBayes <- function(){
   }
   
   print (c (min_error, best_threshold))
-  
   # segment using best_threshold
+  binary <- (image - best_threshold/255) >= 0
+  display(binary, method="raster", all=TRUE)
 }
 
 automaticSegmentationUsingBayes()
