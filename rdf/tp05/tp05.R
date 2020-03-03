7d14d687b9b88cf7094d460e9aa47fb51c405a20
sergiuiacob1 <sergiu.iacob1@gmail.com>
2020-03-03 19:19:17 +0100

    RdF: finish tp05

--- rdf/tp05/tp05.R
+++ rdf/tp05/tp05.R
@@ -37,10 +37,10 @@ calculateClassAPrioriProbabilities <- function(){
   c(pw1, pw2)
 }
 
-# nbins <- 256
-# hCombined <- hist (as.vector (combined), freq=FALSE, breaks = seq (0, 1, 1 / nbins), col=rgb(1,0,0,0.5),  main="combined histogram", xlab="Gray values")
-# h1 <- hist (as.vector (above), freq=FALSE, breaks = seq (0, 1, 1 / nbins), col=rgb(1,0,0,0.5),  main="omega1&omega2 histograms", xlab="Gray values")
-# h2 <- hist (as.vector (below), freq=FALSE, breaks = seq (0, 1, 1 / nbins), col=rgb(0,0,1,0.5), add=T)
+nbins <- 256
+hCombined <- hist (as.vector (combined), freq=FALSE, breaks = seq (0, 1, 1 / nbins), col=rgb(1,0,0,0.5),  main="combined histogram", xlab="Gray values")
+h1 <- hist (as.vector (above), freq=FALSE, breaks = seq (0, 1, 1 / nbins), col=rgb(1,0,0,1),  main="omega1&omega2 histograms", xlab="Gray values")
+h2 <- hist (as.vector (below), freq=FALSE, breaks = seq (0, 1, 1 / nbins), col=rgb(0,0,1,1), add=T)
 
 probs <- calculateClassAPrioriProbabilities()
 print (probs)
@@ -80,13 +80,9 @@ automaticSegmentationUsingBayes <- function(){
   probs <- calculateClassAPrioriProbabilities()
   # only get the imageData, it will be faster for comparisons
   image <- imageData(image)
-  # calculate these dimensions only once
-  dimImage <- dim(image)
-  dimAbove <- dim(above)
-  dimBelow <- dim(below)
   
   # build our reference segmentation
-  perfect <- matrix (ncol=dim(image)[1], nrow=dim(image)[2])
+  perfect <- matrix (nrow=dim(image)[1], ncol=dim(image)[2])
   # dim(image)[1] = number of columns
   for (i in 1:dim(image)[1])
     for (j in 1:dim(above)[2]){
@@ -98,43 +94,39 @@ automaticSegmentationUsingBayes <- function(){
     }
   display(perfect, method="raster", all=TRUE)
   
-  # search for the best threshold
-  # max error is (hypotethically) when all probabilities are 1 and all pixels are missclassified
-  min_error <- 2 * dimImage[1] * dimImage[2]
-  best_threshold <- 0
-  
   # precalculate all probabilities
-  condProbs <- array(dim=c(dimImage[1], dimImage[2], 3))
-  for (i in 1:dimImage[1])
-    for (j in 1:dimImage[2]){
-      res <- calculateConditionalProbability(image[i, j] * 255, probs)
-      condProbs[i, j, 1] <- res[1]
-      condProbs[i, j, 2] <- res[2]
-      condProbs[i, j, 3] <- res[3]
-    }
-      
+  condProbs <- matrix(nrow = 256, ncol = 2)
+  for (X in 0:255){
+    res <- calculateConditionalProbability(X, probs)
+    condProbs[X + 1, 1] <- res[2]
+    condProbs[X + 1, 2] <- res[3]
+  }
+  
+  min_error = 2 * dim(image)[1] * dim(image)[2]
   for (X in 0:255){
     binary <- (image - X/255) >= 0
     # only get the data, otherwise the comparisons will be really slow
     binary <- imageData(binary)
     
     error <- 0
-    for (i in 1:dimImage[1])
-      for (j in 1:dimAbove[2])
-        if (perfect [i, j] != binary [i, j])
-          error <- error + condProbs[2] * probs[1]
-    for (i in 1:dimImage[1])
-      for (j in dimAbove[2]:dimImage[2])
-        if (perfect [i, j] != binary [i, j])
-          error <- error + condProbs[3] * probs[2]
+    # these pixels should be in w1 class
+    for (i in 1:dim(image)[1])
+      for (j in 1:dim(image)[2])
+        if (perfect [i, j] != binary [i, j]){
+          # if it should be in the first class
+          if (j < dim(above)[2])
+            error <- error + condProbs[image[i, j] * 255 + 1, 1] * probs[1]
+          else
+            error <- error + condProbs[image[i, j] * 255 + 1, 2] * probs[2]
+        }
       
     if (error < min_error){
       min_error <- error
       best_threshold <- X
     }
   }
+  print (c(min_error, best_threshold))
   
-  print (c (min_error, best_threshold))
   # segment using best_threshold
   binary <- (image - best_threshold/255) >= 0
   display(binary, method="raster", all=TRUE)
@@ -142,6 +134,142 @@ automaticSegmentationUsingBayes <- function(){
 
 automaticSegmentationUsingBayes()
 
+
+classify0Digit <- function(){
+  # calculate a priori probabilities
+  # P(w) = probability of a pixel to be in the w class
+  # the pixels that are in w class have a gray value of 0 in the image corresponding to w
+  probs <- c(h1$counts[1] / sum(h$counts), h2$counts[1] / sum(h$counts))
+  
+  # only get the imageData, it will be faster for comparisons
+  image <- imageData(image)
+  img_w1 <- imageData(img_w1)
+  img_w2 <- imageData(img_w2)
+  
+  # build our reference segmentation
+  perfect <- matrix (nrow=dim(image)[1], ncol=dim(image)[2])
+  # dim(image)[1] = number of columns
+  for (i in 1:dim(image)[1])
+    for (j in 1:dim(image)[2]){
+      if (img_w1[i, j] == 0){
+        perfect[i, j] <- TRUE
+      }
+      else{
+        perfect[i, j] <- FALSE
+      }
+    }
+  display(perfect, method="raster", all=TRUE)
+  
+  # precalculate all conditional probabilities
+  condProbs <- matrix(nrow = 256, ncol = 2)
+  for (X in 0:255){
+    px <- h$counts[X + 1] / sum(h$counts)
+    # P(X | w) = probability of a pixel having the gray value of X if the pixel is from class w
+    # Using bayes theorem, we get:
+    # P(X|w) = (P(w | X) * P(X)) / P(w)
+    # P(w | X) = the prob. of the pixel being from the w class, if its value is X
+    # 
+    
+    # h1$counts[X + 1] + h2$counts[X + 1] = h$counts[X + 1]
+    # if h1$counts[X + 1] - h$counts[X + 1] is different than 0, then those pixels were NOT assigned to w1
+    
+    if (px == 0){
+      condProbs[X + 1, 1] <- 0
+      condProbs[X + 1, 2] <- 0
+      next
+    }
+    p_w1_if_x <- (h$counts[X + 1] - h1$counts[X + 1]) / h$counts[X + 1]
+    p_x_and_w1 <- p_w1_if_x * px
+    p_x_if_w1 <- p_x_and_w1 / probs[1]
+    p_w2_if_x <- (h$counts[X + 1] - h2$counts[X + 1]) / h$counts[X + 1]
+    p_x_and_w2 <- p_w2_if_x * px
+    p_x_if_w2 <- p_x_and_w2 / probs[2]
+    
+    condProbs[X + 1, 1] <- p_x_if_w1
+    condProbs[X + 1, 2] <- p_x_if_w2
+  }
+  
+  probSum <- 0
+  for (X in 0:255){
+    if(is.nan(condProbs[X + 1, 1]) == FALSE)
+      probSum <- probSum + probs[1] * condProbs[X + 1, 1] + probs[2] * condProbs[X + 1, 2]
+  }
+  
+  print ('Making sure that the sum of conditional probabilities is 1:')
+  print (probSum)
+
+  # search for the best threshold
+  min_error <- 2 * dim(image)[1] * dim(image)[2]
+  best_threshold <- 0
+  for (X in 0:255){
+    binary <- (image - X/255) >= 0
+    # only get the data, otherwise the comparisons will be really slow
+    binary <- imageData(binary)
+    
+    error <- 0
+    for (i in 1:dim(image)[1])
+      for (j in 1:dim(image)[2])
+        if (perfect [i, j] != binary [i, j]){
+          # if it should have been in w1
+          if (img_w1[i, j] == 0)
+            error <- error + condProbs[image[i, j] * 255 + 1, 1] * probs[1]
+          else
+            # it should have been in w1
+            error <- error + condProbs[image[i, j] * 255 + 1, 2] * probs[2]
+        }
+    
+    if (error < min_error){
+      min_error <- error
+      best_threshold <- X
+    }
+  }
+  print (c (min_error, best_threshold))
+  # segment using best_threshold
+  binary <- (image - best_threshold / 255) >= 0
+  display(binary, method="raster", all=TRUE)
+  best_threshold
+}
+
+# get img, w1 and w2 histograms
+image <- rdfReadGreyImage("rdf-chiffre-0-8bits.png")
+img_w1 <- rdfReadGreyImage("rdf-chiffre-0-8bits_omega1.png")
+img_w2 <- rdfReadGreyImage("rdf-chiffre-0-8bits_omega2.png")
+nbins <- 256
+h <- hist (as.vector (image), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
+h1 <- hist (as.vector (img_w1), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
+h2 <- hist (as.vector (img_w2), freq=FALSE, breaks = seq (0, 1, 1 / nbins))
+
+best_threshold <- classify0Digit()
+
+image <- rdfReadGreyImage("rdf-chiffre-1-8bits.png")
+binary <- (image - best_threshold/255) < 0
+display(binary, method="raster", all=TRUE)
+
+
+# Calculate error rate
+image <- rdfReadGreyImage("rdf-chiffre-0-8bits.png")
+best_threshold <- classify0Digit()
+binary <- (image - best_threshold / 255) >= 0
+# build our reference segmentation
+perfect <- matrix (nrow=dim(image)[1], ncol=dim(image)[2])
+# dim(image)[1] = number of columns
+for (i in 1:dim(image)[1])
+  for (j in 1:dim(image)[2]){
+    if (img_w1[i, j] == 0){
+      perfect[i, j] <- TRUE
+    }
+    else{
+      perfect[i, j] <- FALSE
+    }
+  }
+sprintf("Error for digit 0: %f", sum(abs(perfect - binary)) / (dim(perfect)[1] * dim(perfect)[2]))
+
+# calculate error rate for 1
+image <- rdfReadGreyImage("rdf-chiffre-1-8bits.png")
+binary <- (image - best_threshold / 255) < 0
+perfect <- rdfReadGreyImage("rdf-chiffre-1-8bits_classe_a_trouver.png")
+display(binary, method="raster", all=TRUE)
+sprintf("Error for digit 1: %f", sum(abs(perfect - binary)) / (dim(perfect)[1] * dim(perfect)[2]))
 
 
 
