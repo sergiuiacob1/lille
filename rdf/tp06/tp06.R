f3ad87c688c9c46a8a22b63e683328f01f161e65
sergiuiacob1 <sergiu.iacob1@gmail.com>
2020-03-11 11:34:53 +0100

    RdF: TP6 report work

--- rdf/tp06/tp06.R
+++ rdf/tp06/tp06.R
@@ -17,10 +17,14 @@ plot (x_test, col=couleur, main='Test data', xlab='First feature', ylab='Second 
 
 # For training data: see a priori probabilities
 # P(w) = probability of an instance to be in the class "w"
-class_probs <- 1:3
-for (class_no in 1:3)
-  class_probs[class_no] = sum(couleur == class_colors[class_no]) / length(couleur)
-class_probs
+class_probs_app <- 1:3
+class_probs_test <- 1:3
+for (class_no in 1:3){
+  class_probs_app[class_no] = sum(class_no == classe_app) / length(classe_app)
+  class_probs_test[class_no] = sum(class_no == classe_test) / length(classe_test)
+}
+class_probs_app
+class_probs_test
 
 # 1.2
 # calculate means
@@ -40,7 +44,7 @@ sigma <- c()
 for (class_no in 1:3){
   # number of instances = n_app = dim(x_app)[1]
   # number of features = dim(x_app)[2]
-  sigma[[class_no]] <- matrix(0, dim(x_app)[2], dim(x_app)[2])
+  sigma[[class_no]] <- matrix(1, dim(x_app)[2], dim(x_app)[2])
   for (i in 1:dim(x_app)[2])
     for (j in 1:dim(x_app)[2])
       sigma[[class_no]][i, j] <- cov(as.vector(x_app[classe_app == class_no, i]), as.vector (x_app[classe_app == class_no, j]))
@@ -86,4 +90,12 @@ forms <- 1:n_test
 possible_forms <- c(15, 17, 19)
 for (i in 1:n_test)
   forms[i] = possible_forms[assigne_test$class[i]]
+
+# Plot lines and points again
+plot(grille)
+zp<-assigne_test$post[,3]-pmax(assigne_test$post[,2],assigne_test$post[,1])
+contour(xp1,xp2,matrix(zp,50),add=TRUE,levels=0,drawlabels=FALSE)
+zp<-assigne_test$post[,2]-pmax(assigne_test$post[,3],assigne_test$post[,1])
+contour(xp1,xp2,matrix(zp,50),add=TRUE,levels=0,drawlabels=FALSE)
 points(x_test, col=couleur, p=forms)
+
